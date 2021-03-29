e5df9e4c3ab85bf1aa570f12d86280fc068f3747
artzaza7 <75871892+artzaza7@users.noreply.github.com>
2021-03-29 18:30:10 +0700

    Final_1 ได้ 70 ครับ

--- 114_Final_1.c
+++ 114_Final_1.c
@@ -3,23 +3,31 @@ int main()
 {
     int n,c;
     scanf("%d %d",&n,&c);
-    int k=n,save=k%10;
-    if(save==c)
+    int k=n+1,save=k%10;
+    int count=0;
+    int key=k;
+    while(key>0)
     {
-        k++;
-        save=k%10;
+        count++;
+        key=key/10;
     }
-    while(1)
+    int give=count;
+    int temp=k;
+    while (count>0)
     {
+        if(count==1)
+        {
+            k++;
+            temp=k;
+            count=give;
+        }
+        save=temp%10;
         if(save==c)
         {
             break;
         }
-        else
-        {
-            k++;
-            save=k%10;
-        }
+        temp=temp/10;
+        count--;
     }
     printf("%d",k);
     return 0;
