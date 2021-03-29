f37e4c7273f7cdfc7ae377b1fd8e8961447a1502
artzaza7 <75871892+artzaza7@users.noreply.github.com>
2021-03-29 19:37:58 +0700

    Final_3 เก็บได้ทุกอย่างแต่ ไม่เต็ม 100

--- 114_Final_3.c
+++ 114_Final_3.c
@@ -4,124 +4,257 @@ int main()
     int n1,n2,n3;
     scanf("%d %d %d",&n1,&n2,&n3);
     int k1[n1][n1],k2[n2][n2],k3[n3][n3];
-    int sum1[n1+n1],sum2[n2+n2],sum3[n3+n3];
-    for(int i=0;i<n1+n1;i++)
+    int a=n1*2+2,b=n2*2+2,c=n3*2+2;
+    int sum1[a],sum2[b],sum3[c];
+    for(int i=0;i<a;i++)
     {
         sum1[i]=0;
     }
-    for(int i=0;i<n2+n2;i++)
+    for(int i=0;i<b;i++)
     {
         sum2[i]=0;
     }
-    for(int i=0;i<n3+n3;i++)
+    for(int i=0;i<c;i++)
     {
         sum3[i]=0;
     }
-    int sum=0;
-    int k=0;
+    int add=0;
     for(int i=0;i<n1;i++)
     {
         for(int j=0;j<n1;j++)
         {
             scanf("%d",&k1[i][j]);
-            sum=sum+k1[i][j];
         }
-        sum1[k]=sum;
-        sum=0;
-        k++;
     }
-    for(int i=0;i<n1+n1;i++)
-    {
-        printf("%d ",sum1[i]);
-    }
-    sum=0;
-    k=0;
     for(int i=0;i<n2;i++)
     {
         for(int j=0;j<n2;j++)
         {
             scanf("%d",&k2[i][j]);
-            sum=sum+k2[i][j];
         }
-        sum2[k]=sum;
-        sum=0;
-        k++;
     }
-    for(int i=0;i<n2+n2;i++)
-    {
-        printf("%d ",sum2[i]);
-    }
-    sum=0;
-    k=0;
     for(int i=0;i<n3;i++)
     {
         for(int j=0;j<n3;j++)
         {
             scanf("%d",&k3[i][j]);
-            sum=sum+k3[i][j];
         }
-        sum3[k]=sum;
-        sum=0;
+    }
+    int x=a-2,y=b-2,z=c-2;
+    int k=0;
+    while(k<a)
+    {
+        if(k<x/2)
+        {
+            for(int i=0;i<n1;i++)
+            {
+                for(int j=0;j<n1;j++)
+                {
+                    sum1[k]=sum1[k]+k1[i][j];
+                }
+                k++;
+            }
+        }
+        else if(k<x)
+        {
+            k--;
+            for(int i=0;i<n1;i++)
+            {
+                for(int j=0;j<n1;j++)
+                {
+                    sum1[k]=sum1[k]+k1[j][i];
+                }
+                k++;
+            }
+        }
+        if(k==x)
+        {
+            for(int i=n1-1;i>=0;i--)
+            {
+                for(int j=n1-1;j>=0;j--)
+                {
+                    if(i==j)
+                    {
+                        sum1[k]=sum1[k]+k1[i][j];
+                    }
+                }
+            }
+        }
+        else
+        {
+            for(int i=0;i<n1;i++)
+            {
+                for(int j=0;j<n1;j++)
+                {
+                    if(i==j)
+                    {
+                        sum1[k]=sum1[k]+k1[i][j];
+                    }
+                }
+            }
+        }
         k++;
     }
-    for(int i=0;i<n3+n3;i++)
+    k=0;
+    while(k<c)
     {
-        printf("%d ",sum3[i]);
+        if(k<z/2)
+        {
+            for(int i=0;i<n1;i++)
+            {
+                for(int j=0;j<n1;j++)
+                {
+                    sum3[k]=sum3[k]+k3[i][j];
+                }
+                k++;
+            }
+        }
+        else if(k<z)
+        {
+            k--;
+            for(int i=0;i<n3;i++)
+            {
+                for(int j=0;j<n3;j++)
+                {
+                    sum3[k]=sum3[k]+k3[j][i];
+                }
+                k++;
+            }
+        }
+        if(k==z)
+        {
+            for(int i=n3-1;i>=0;i--)
+            {
+                for(int j=n3-1;j>=0;j--)
+                {
+                    if(i==j)
+                    {
+                        sum3[k]=sum3[k]+k3[i][j];
+                    }
+                }
+            }
+        }
+        else
+        {
+            for(int i=0;i<n3;i++)
+            {
+                for(int j=0;j<n3;j++)
+                {
+                    if(i==j)
+                    {
+                        sum3[k]=sum3[k]+k3[i][j];
+                    }
+                }
+            }
+        }
+        k++;
     }
-    /*
-    int max1=sum1[0],max2=sum2[0],max3=sum3[0];
-    int count=0;
-    for(int i=1;i<n1+n1;i++)
+    k=0;
+    while(k<b)
     {
-        if(max1!=sum1[i])
+        if(k<y/2)
+        {
+            for(int i=0;i<n2;i++)
+            {
+                for(int j=0;j<n2;j++)
+                {
+                    sum2[k]=sum2[k]+k2[i][j];
+                }
+                k++;
+            }
+        }
+        else if(k<y)
+        {
+            k--;
+            for(int i=0;i<n2;i++)
+            {
+                for(int j=0;j<n2;j++)
+                {
+                    sum2[k]=sum2[k]+k2[j][i];
+                }
+                k++;
+            }
+        }
+        if(k==y)
+        {
+            for(int i=n2-1;i>=0;i--)
+            {
+                for(int j=n2-1;j>=0;j--)
+                {
+                    if(i==j)
+                    {
+                        sum2[k]=sum2[k]+k2[i][j];
+                    }
+                }
+            }
+        }
+        else
+        {
+            for(int i=0;i<n2;i++)
+            {
+                for(int j=0;j<n2;j++)
+                {
+                    if(i==j)
+                    {
+                        sum2[k]=sum2[k]+k2[i][j];
+                    }
+                }
+            }
+        }
+        k++;
+    }
+    k=0;
+    int count=0,max1=sum1[0],max2=sum2[0],max3=sum3[0];
+    for(int i=0;i<a;i++)
+    {
+        if(max1==sum1[i])
         {
             count++;
         }
     }
-    if(count!=0)
-    {
-        printf("yes\n");
-        count=0;
-    }
-    else
+    if(count==a-1)
     {
         printf("no\n");
         count=0;
     }
-    for(int i=1;i<n2+n2;i++)
+    else
     {
-        if(max2!=sum2[i])
+        printf("yes\n");
+        count=0;
+    }
+    for(int i=0;i<b;i++)
+    {
+        if(max2==sum2[i])
         {
             count++;
         }
     }
-    if(count!=0)
-    {
-        printf("yes\n");
-        count=0;
-    }
-    else
+    if(count==b-1)
     {
         printf("no\n");
         count=0;
     }
-    for(int i=1;i<n3+n3;i++)
+    else
     {
-        if(max3!=sum3[i])
+        printf("yes\n");
+        count=0;
+    }
+    for(int i=0;i<c;i++)
+    {
+        if(max3==sum3[i])
         {
             count++;
         }
     }
-    if(count!=0)
-    {
-        printf("yes\n");
-        count=0;
-    }
-    else
+    if(count==c-1)
     {
         printf("no\n");
         count=0;
     }
-    */
+    else
+    {
+        printf("yes\n");
+        count=0;
+    }
     return 0;
 }
