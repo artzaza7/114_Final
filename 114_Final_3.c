0ec003ab8074bedb2f5508ca0a196ab13a717322
artzaza7 <75871892+artzaza7@users.noreply.github.com>
2021-03-29 17:20:21 +0700

    Final_3 เก็บ ข้อมูล

---
+++ 114_Final_3.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int n1,n2,n3;
+    scanf("%d %d %d",&n1,&n2,&n3);
+    int k1[n1][n1],k2[n2][n2],k3[n3][n3];
+    for(int i=0;i<n1;i++)
+    {
+        for(int j=0;j<n1;j++)
+        {
+            scanf("%d",&k1[i][j]);
+        }
+    }
+    for(int i=0;i<n2;i++)
+    {
+        for(int j=0;j<n2;j++)
+        {
+            scanf("%d",&k2[i][j]);
+        }
+    }
+    for(int i=0;i<n3;i++)
+    {
+        for(int j=0;j<n3;j++)
+        {
+            scanf("%d",&k3[i][j]);
+        }
+    }
+    return 0;
+}
